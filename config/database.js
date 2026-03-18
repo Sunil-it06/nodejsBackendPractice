60fe5a8d557f664f5a79da06f9b65ba1fb2b7968
Sunil-it06 <sunilguptait06@gmail.com>
2026-03-19 01:53:06 +0530

    fix production db config

--- config/database.js
+++ config/database.js
@@ -1,21 +1,39 @@
+// const { Sequelize } = require("sequelize");
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME || "emergency_db",
+//   process.env.DB_USER || "root",
+//   process.env.DB_PASS || "",
+//   {
+//     host: process.env.DB_HOST || "localhost",
+//     dialect: "mysql",
+
+//     logging: false,
+
+//     pool: {
+//       max: 10,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     }
+//   }
+// );
+
+// module.exports = sequelize;
+
+
+
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "emergency_db",
-  process.env.DB_USER || "root",
-  process.env.DB_PASS || "",
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
   {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     dialect: "mysql",
-
     logging: false,
-
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
   }
 );
 
